ca980c18d547d83150504b0d75925306a21239ae
Nuno Dias <nuno.jaco@gmail.com>
2021-10-26 11:44:11 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,38 @@
+module github.com/days85/shipping_tracking
+
+go 1.17
+
+require github.com/days85/shipping_cargo v0.0.1
+
+require (
+	github.com/days85/shipping_inmem v0.0.1
+	github.com/days85/shipping_routing v0.0.1
+	github.com/go-kit/kit v0.12.0
+	github.com/gorilla/mux v1.8.0
+	github.com/prometheus/client_golang v1.11.0
+)
+
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/days85/shipping_location v0.0.1 // indirect
+	github.com/days85/shipping_voyage v0.0.1 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20211023200351-1e6abe791855 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/smartystreets/assertions v1.2.1 // indirect
+	github.com/sony/gobreaker v0.4.1 // indirect
+	github.com/streadway/handy v0.0.0-20200128134331-0f66f006fb2e // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+    "context"
+    "flag"
+    "fmt"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+
+	`github.com/days85/shipping_tracking/tracking`
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+
+    "github.com/go-kit/kit/log"
+    kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+
+    inmem "github.com/days85/shipping_inmem"
+    routing "github.com/days85/shipping_routing"
+)
+
+const (
+	defaultPort              = "8082"
+	defaultRoutingServiceURL = "http://localhost:7878"
+)
+
+func main() {
+	var (
+		addr  = envString("PORT", defaultPort)
+		rsurl = envString("ROUTINGSERVICE_URL", defaultRoutingServiceURL)
+
+		httpAddr          = flag.String("http.addr", ":"+addr, "HTTP listen address")
+		routingServiceURL = flag.String("service.routing", rsurl, "routing service URL")
+
+		ctx = context.Background()
+	)
+
+	flag.Parse()
+
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+
+	var (
+		cargos         = inmem.NewCargoRepository()
+		handlingEvents = inmem.NewHandlingEventRepository()
+	)
+
+	fieldKeys := []string{"method"}
+
+	var rs routing.Service
+	rs = routing.NewProxyingMiddleware(ctx, *routingServiceURL)(rs)
+
+	var ts tracking.Service
+	ts = tracking.NewService(cargos, handlingEvents)
+	ts = tracking.NewLoggingService(log.With(logger, "component", "tracking"), ts)
+	ts = tracking.NewInstrumentingService(
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "tracking_service",
+			Name:      "request_count",
+			Help:      "Number of requests received.",
+		}, fieldKeys),
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "tracking_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, fieldKeys),
+		ts,
+	)
+
+	httpLogger := log.With(logger, "component", "http")
+
+	mux := http.NewServeMux()
+	mux.Handle("/tracking/v1/", tracking.MakeHandler(ts, httpLogger))
+
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+	go func() {
+		logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+		errs <- http.ListenAndServe(*httpAddr, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Log("terminated", <-errs)
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
+}
